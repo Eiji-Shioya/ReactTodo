6ac9fdfbff53ba8e4f22a5e0d24da6879a5db3f4
Eiji Shioya <69416353+Eiji-Shioya@users.noreply.github.com>
2021-03-05 10:01:00 +0700

    1

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import react from "react";
+import "./styles.css";
+
+export const App = () => {
+  return (
+    <>
+      <div>
+        <input placeholder="Todo input area" />
+        <button>Add</button>
+      </div>
+      <div>
+        <p>incomplete</p>
+        <ul>
+          <div>
+            <li>aaaa</li>
+            <button>complete</button>
+            <button>delete</button>
+          </div>
+          <div>
+            <li>bbbb</li>
+            <button>complete</button>
+            <button>delete</button>
+          </div>
+        </ul>
+      </div>
+      <div>
+        <p>complete</p>
+        <ul>
+          <div>
+            <li>ccc</li>
+            <button>return</button>
+          </div>
+        </ul>
+      </div>
+    </>
+  );
+};
