1f31812338a7881039c3e78a85c2f04ce8b203f0
Eiji Shioya <69416353+Eiji-Shioya@users.noreply.github.com>
2021-03-05 14:12:18 +0700

    4

--- src/App.jsx
+++ src/App.jsx
@@ -8,25 +8,27 @@ export const App = () => {
         <input placeholder="Todo input area" />
         <button>Add</button>
       </div>
-      <div>
-        <p>incomplete</p>
+
+      <div className="incomplete-area">
+        <p className="title">incomplete</p>
         <ul>
-          <div>
+          <div className="list-row">
             <li>aaaa</li>
             <button>complete</button>
             <button>delete</button>
           </div>
-          <div>
+          <div className="list-row">
             <li>bbbb</li>
             <button>complete</button>
             <button>delete</button>
           </div>
         </ul>
       </div>
-      <div>
-        <p>complete</p>
+
+      <div className="complete-area">
+        <p className="title">complete</p>
         <ul>
-          <div>
+          <div className="list-row">
             <li>ccc</li>
             <button>return</button>
           </div>
